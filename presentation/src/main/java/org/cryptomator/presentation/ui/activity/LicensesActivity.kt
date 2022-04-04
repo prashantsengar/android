ac45877068c238d421632a4a22dbb969ac6365a5
Prashant Sengar <45726744+prashantsengar@users.noreply.github.com>
2022-04-04 11:12:10 +0530

    Update LicensesActivity.kt

--- presentation/src/main/java/org/cryptomator/presentation/ui/activity/LicensesActivity.kt
+++ presentation/src/main/java/org/cryptomator/presentation/ui/activity/LicensesActivity.kt
@@ -1,19 +1,19 @@
-package org.cryptomator.presentation.ui.activity
+// package org.cryptomator.presentation.ui.activity
 
-import org.cryptomator.generator.Activity
-import org.cryptomator.presentation.R
-import kotlinx.android.synthetic.main.toolbar_layout.toolbar
+// import org.cryptomator.generator.Activity
+// import org.cryptomator.presentation.R
+// import kotlinx.android.synthetic.main.toolbar_layout.toolbar
 
-@Activity(layout = R.layout.activity_licenses)
-class LicensesActivity : BaseActivity() {
+// @Activity(layout = R.layout.activity_licenses)
+// class LicensesActivity : BaseActivity() {
 
-	override fun setupView() {
-		setupToolbar()
-	}
+// 	override fun setupView() {
+// 		setupToolbar()
+// 	}
 
-	private fun setupToolbar() {
-		toolbar.setTitle(R.string.screen_licenses_title)
-		setSupportActionBar(toolbar)
-	}
+// 	private fun setupToolbar() {
+// 		toolbar.setTitle(R.string.screen_licenses_title)
+// 		setSupportActionBar(toolbar)
+// 	}
 
-}
+// }
